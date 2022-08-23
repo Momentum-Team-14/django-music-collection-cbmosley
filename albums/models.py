44aa5f6efb68f40ad4fb978a0e78b7a18e921388
cbmosley <cbmosley@ncsu.edu>
2022-08-23 11:35:41 -0400

    Homepage shows Albums added from admin page.

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} by {self.artist}'
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Album
 # Create your views here.
+
+
+def list_albums(request):
+    albums = Album.objects.all()
+    return render(request, 'albums/album_home.html', {"albums": albums})
